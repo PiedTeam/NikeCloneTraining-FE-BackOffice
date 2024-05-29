b7d47cd437d800576f7d994103e603c910a57f6c
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2024-05-30 04:07:19 +0700

    feat: init source code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { NextUIProvider } from "@nextui-org/react";
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <NextUIProvider>
+      <h1 className="text-red-500">ahihi</h1>
+    </NextUIProvider>
+  );
+}
+
+export default App;
