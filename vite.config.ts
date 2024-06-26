2238488c5c43837c7b11f7fbf07a2a52d27b954e
Nguyễn Nam Hoàng <nnh53.work@gmail.com>
2024-06-26 18:36:49 +0700

    Feature: new eslint and github action CI (#5)
    * feat: new eslint and github action CI
    
    * chore: update lock file
    
    * fix: typo on CI
    
    * feat: update ci env
    
    * chore: fix typo
    
    ---------
    
    Co-authored-by: nnh53 <“nnh53.work@gmail.com”>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig: UserConfig = {
   plugins: [react()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   css: {
     devSourcemap: true,
   },
-});
+  resolve: {
+    alias: {
+      "@apis": "/src/apis",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@constants": "/src/constants",
+      "@contexts": "/src/contexts",
+      "@hooks": "/src/hooks",
+      "@layout": "/src/layout",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+      "@services": "/src/services",
+      "@stores": "/src/stores",
+      "@utils": "/src/utils",
+      "@public": "/public",
+      "@common": "/src/common",
+      "@provider": "/src/provider",
+      "@routes": "/src/routes",
+    },
+  },
+};
+
+export default defineConfig(viteConfig);
